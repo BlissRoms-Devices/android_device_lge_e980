bf31075e747fcc1324a834b42513163b814d781b
nerdyblonde <krisnicbell@gmail.com>
2015-11-03 12:20:41 -0800

    Update & Cleanup
    *Use some props from @ShadySquirrel with
    his permission.  Thanks to him for all his work!
    
    Merge some of the gproj-common props
    
    Do some more cleanup
    
    Delete thermald since using Intellithermal

--- device.mk
+++ device.mk
@@ -33,18 +33,29 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 PRODUCT_PACKAGES += \
 	lights.geefhd \
 	camera.geefhd
+	
+# Live Wallpapers
+PRODUCT_PACKAGES += \
+	LiveWallpapers \
+	LiveWallpapersPicker \
+	VisualizationWallpapers \
+	librs_jni
 
 PRODUCT_PACKAGES += \
 	hostapd_default.conf \
 	wpa_supplicant_overlay.conf \
 	p2p_supplicant_overlay.conf
+	
+PRODUCT_PACKAGES += \
+	libwpa_client \
+	hostapd \
+	dhcpcd.conf \
+	wpa_supplicant \
+	wpa_supplicant.conf
 
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/mixer_paths.xml:system/etc/mixer_paths.xml
-
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/thermald-geefhd.conf:system/etc/thermald.conf
-
+	device/lge/e980/mixer_paths.xml:system/etc/mixer_paths.xml
+	
 # Add bluetooth configuration from stock
 PRODUCT_COPY_FILES += \
 	device/lge/e980/bluetooth_extras/etc/audio.conf:system/etc/bluetooth/audio.conf \
@@ -62,11 +73,11 @@ PRODUCT_COPY_FILES += \
 	device/lge/e980/bluetooth_extras/etc/input.conf:system/etc/bluetooth/input.conf \
 	device/lge/e980/bluetooth_extras/etc/iop_bt.db:system/etc/bluetooth/iop_bt.db \
 	device/lge/e980/bluetooth_extras/etc/main.conf:system/etc/bluetooth/main.conf \
-	device/lge/e980/bluetooth_extras/etc/network.conf:system/etc/bluetooth/network.conf \
+	device/lge/e980/bluetooth_extras/etc/network.conf:system/etc/bluetooth/network.conf
 
 # Add bluetooth framework file...
 PRODUCT_COPY_FILES += \
-	device/lge/e980/bluetooth_extras/framework/com.broadcom.bt.jar:system/framework/com.broadcom.bt.jar \
+	device/lge/e980/bluetooth_extras/framework/com.broadcom.bt.jar:system/framework/com.broadcom.bt.jar
 
 # Add bluetooth libs and binaries. Those should be in vendor/lge/e980, but that one needs some updating...
 PRODUCT_COPY_FILES += \
@@ -75,13 +86,16 @@ PRODUCT_COPY_FILES += \
 	device/lge/e980/bluetooth_extras/lib/libbluetoothsettings_jni.so:system/lib/libbluetoothsettings_jni.so \
 	device/lge/e980/bluetooth_extras/xbin/hciconfig:system/xbin/hciconfig \
 	device/lge/e980/bluetooth_extras/xbin/hcidump:system/xbin/hcidump \
-	device/lge/e980/bluetooth_extras/xbin/hcitool:system/xbin/hcitool \
+	device/lge/e980/bluetooth_extras/xbin/hcitool:system/xbin/hcitool
 	   
-# Bluetooth additional config...
+# Additional Bluetooth Configs
 PRODUCT_PROPERTY_OVERRIDES += \
 	bluetooth.chip.vendor=brcm \
+	ro.bluetooth.request.master=true \
+	ro.bluetooth.remote.autoconnect=true \
+	ro.bluetooth.PANenable = 1 \
+	bluetooth.a2dp.sink.enabled=false \
 	ro.product.bluetooth=4.0 \
-	ro.bt.chipset=Broadcom BCM4334 \
 	ro.bt.version=4.0 \
 	ro.bt.stack=BTL-A \
 	ro.bt.stack.version=5.0 \
@@ -93,25 +107,25 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	service.brcm.bt.btld_pid=0 \
 	service.brcm.bt.avrcp_pass_thru=0 \
 	service.brcm.bt.avrcp_toggle=1 \
-	service.brcm.bt.btport_redir_on=1 \
+	service.brcm.bt.btport_redir_on=1
 
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/initlogo.rle:root/initlogo.rle888 \
-	$(LOCAL_PATH)/init.geefhd.rc:root/init.geefhd.rc \
-	$(LOCAL_PATH)/fstab.geefhd:root/fstab.geefhd \
-	$(LOCAL_PATH)/ueventd.geefhd.rc:root/ueventd.geefhd.rc
+	device/lge/e980/initlogo.rle:root/initlogo.rle888 \
+	device/lge/e980/init.geefhd.rc:root/init.geefhd.rc \
+	device/lge/e980/fstab.geefhd:root/fstab.geefhd \
+	device/lge/e980/ueventd.geefhd.rc:root/ueventd.geefhd.rc
 
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/recovery/postrecoveryboot.sh:recovery/root/sbin/postrecoveryboot.sh
+	device/lge/e980/recovery/postrecoveryboot.sh:recovery/root/sbin/postrecoveryboot.sh
 
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/keypad_8064.kl:system/usr/keylayout/gk-keypad-8064.kl
+	device/lge/e980/keypad_8064.kl:system/usr/keylayout/gk-keypad-8064.kl
 
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/bcmdhd.cal:system/etc/wifi/bcmdhd.cal
-
-PRODUCT_PROPERTY_OVERRIDES += \
-	ro.sf.lcd_density=480
+	device/lge/e980/bcmdhd.cal:system/etc/wifi/bcmdhd.cal
+	
+PRODUCT_COPY_FILES += \
+	frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
 
 PRODUCT_PACKAGES += \
 	hwaddrs
@@ -125,17 +139,72 @@ PRODUCT_PACKAGES += \
 	loki_tool_static_gproj \
 	recovery-transform.sh
 
-PRODUCT_COPY_FILES += \
-	frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
-
-PRODUCT_PROPERTY_OVERRIDES += \
-	ro.telephony.default_network=9 \
-	telephony.lteOnGsmDevice=1
-
 PRODUCT_PROPERTY_OVERRIDES += \
 	pm.sleep.mode=1 \
 	persist.sys.language=en-US \
-	persist.sys.country=US
+	persist.sys.country=US \
+	camera2.portability.force_api=1 \
+	ro.sf.lcd_density=480
+	
+# Audio Configuration
+# FIXME: Remove persist.audio.handset.mic and persist.audio.fluence.mode
+#        while switching new audio HAL from legacy HAL
+PRODUCT_PROPERTY_OVERRIDES += \
+	persist.audio.handset.mic.type=digital \
+	persist.audio.dualmic.config=endfire \
+	persist.audio.fluence.voicecall=true \
+	persist.audio.handset.mic=dmic \
+	persist.audio.fluence.mode=endfire \
+	persist.audio.lowlatency.rec=false
+
+# Enable AAC 5.1 output
+PRODUCT_PROPERTY_OVERRIDES += \
+    media.aac_51_output_enabled=true
+
+# Do not power down SIM card when modem is sent to Low Power Mode.
+PRODUCT_PROPERTY_OVERRIDES += \
+	persist.radio.apm_sim_not_pwdn=1
+
+# RIL
+	PRODUCT_PROPERTY_OVERRIDES += \
+	ro.telephony.default_network=9 \
+	telephony.lteOnGsmDevice=1 \
+	ro.telephony.ril_class=LgeLteRIL \
+	ro.telephony.ril.config=qcomdsds
+	rild.libpath=/system/lib/libril-qc-qmi-1.so \
+	ro.telephony.call_ring.multiple=0
+	
+# Wifi Configs
+PRODUCT_PROPERTY_OVERRIDES += \
+	wifi.interface=wlan0 \
+	wifi.supplicant_scan_interval=15 \
+	ro.wifi.channels=11 11
+
+# More overrides from stock build.prop
+PRODUCT_PROPERTY_OVERRIDES += \
+	debug.sf.hw=1 \
+	debug.egl.hw=1 \
+	video.accelerate.hw=1 \
+	wlan.chip.vendor=brcm \
+	wlan.chip.version=bcm4334 \
+	af.resampler.quality=255
+	
+# Random overrides
+PRODUCT_PROPERTY_OVERRIDES += \
+	debug.performance.tuning=1
+	
+PRODUCT_PROPERTY_OVERRIDES += \
+	debug.egl.recordable.rgba8888=1
+
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.qc.sensors.wl_dis=true \
+	ro.qualcomm.sensors.smd=true
+
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+	persist.sys.usb.config=mtp
+	
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.opengles.version=196608
 	
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
